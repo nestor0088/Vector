3070d9c1935ada5893664570f0955bbb0543df09
HPLAB202 <HPLAB202@WRH-000313-L017>
2023-10-31 10:17:33 -0500

    Add project files.

---
+++ Vector/Vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+	vector<int> numbers = { 23,56,78,89,90 };
+	cout << "Original Vector" << endl;
+	for (int x = 0; x < numbers.size(); x++)
+	{
+		cout << numbers[x] << endl;
+	}
+	numbers.insert(numbers.begin() + 2, 100);
+	cout << "\n\nCurrent Vector" << endl;
+	for (int x = 0; x < numbers.size(); x++)
+	{
+		cout << numbers[x] << endl;
+	}
+	numbers.erase(numbers.begin(), numbers.end() - 2);
+	cout << "\n\nCurrent Vector" << endl;
+	for (int x = 0; x < numbers.size(); x++)
+	{
+		cout << numbers[x] << endl;
+	}
+}
